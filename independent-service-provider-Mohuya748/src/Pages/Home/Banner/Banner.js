66fcb7f80ba49caa0c645ea99c12f5df202a9af3
Mohuya748 <khandakermohuya@gmail.com>
2022-04-19 21:21:36 +0600

    changed

---
+++ independent-service-provider-Mohuya748/src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div class="container mt-4 mb-4">
+            <div class="row">
+                <div class="col-lg-6 m-3">
+                    <h1>MOHU'S PHOTOGRAPHYddd</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque iusto deserunt recusandae autem, quam ipsum nam ratione placeat ex doloremque. Quasi, velit? Ipsum officia quas velit a praesentium, libero quisquam?</p>
+                </div>
+                <div class="col-lg-6 m-3">
+                    <img src="top.jpg" alt="top" style={{ width: '400px' }} className='m-3' />
+                </div>
+            </div>
+            </div>
+            );
+};
+
+            export default Banner;
